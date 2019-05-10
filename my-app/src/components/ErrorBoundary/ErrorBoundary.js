f6de043020bdba0bce08b06be857c5aee9956247
Alex Lazareva <alex.lazareva25@yandex.ru>
2019-05-10 20:50:29 +0300

    change project configuration

--- my-app/src/components/ErrorBoundary/ErrorBoundary.js
+++ my-app/src/components/ErrorBoundary/ErrorBoundary.js
@@ -9,7 +9,7 @@ export default class ErrorBoundary extends React.Component {
     }
     render() {
         if (this.state.hasError) {
-            return (<h1>Something error</h1>);
+            return <h1>Something error</h1>;
         }
         return this.props.children;
     }
--- my-app/src/containers/App/App.js
+++ my-app/src/containers/App/App.js
@@ -1,68 +1,31 @@
 import React, { Component } from 'react';
-import Car from './../../components/Car/Car';
-import ErrorBoundary from './../../components/ErrorBoundary/ErrorBoundary';
+import Layout from './../../components/hoc/Layout/Layout'
+// import ErrorBoundary from './../../components/ErrorBoundary/ErrorBoundary';
 import './App.scss';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
+    // constructor(props) {
+    //     super(props);
+    //
+    //     this.state = {
+    //         cars: [
+    //             {name: 'Audi', year: 2016},
+    //             {name: 'Ford', year: 2018},
+    //             {name: 'Mazda', year: 2013}
+    //         ],
+    //         pageTitle: 'React components',
+    //         showCars: false
+    //     };
+    // }
 
-        this.state = {
-            cars: [
-                {name: 'Audi', year: 2016},
-                {name: 'Ford', year: 2018},
-                {name: 'Mazda', year: 2013}
-            ],
-            pageTitle: 'React components',
-            showCars: false
-        };
-    }
-
-// App handlers
-    toggleCarsHandler = () => {
-        this.setState({
-            showCars: !this.state.showCars
-        });
-    };
-// Car component functions
-    onChangeName = (inputValue, index) => {
-        const car = this.state.cars[index];
-        const cars = [...this.state.cars];
-
-        car.name = inputValue;
-        cars[index] = car;
-
-        this.setState({cars});
-    };
-
-    deleteItem(index) {
-        const cars = this.state.cars.concat();
-
-        cars.splice(index, 1);
-        this.setState({cars});
-    };
 
 // render App
   render() {
     return (
       <div className="App">
-        <h1>{this.state.pageTitle}</h1>
-        <button onClick={this.toggleCarsHandler}>Toggle cars</button>
-          {this.state.showCars ?
-              this.state.cars.map((car, index) => {
-                  return (
-                      <ErrorBoundary key={index}>
-                          <Car
-                              name={car.name}
-                              year={car.year}
-                              onChangeName = {event => {this.onChangeName(event.target.value, index)}}
-                              onDelete={this.deleteItem.bind(this, index)}
-                          />
-                      </ErrorBoundary>
-                  );
-              })
-              : null
-          }
+          <Layout>
+              <h1>Layout</h1>
+          </Layout>
       </div>
     );
   }
